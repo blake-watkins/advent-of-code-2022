127173af7025613446af578ee5ca8a1b61a879c1
Blake Watkins <blakewatkins@gmail.com>
2022-12-01 18:09:13 +1300

    Submit Day 1

--- day1.lisp
+++ day1.lisp
@@ -1,1 +1,18 @@
 (in-package :aoc-2022)
+
+(defun parse-elf ()
+  (with-monad
+    (assign cals (parse-list (parse-number) (parse-newline)))
+    (unit (reduce #'+ cals))))
+
+(defun parse-elves ()
+  (parse-list (parse-elf) (with-monad (parse-newline) (parse-newline))))
+
+(defun day1 (input)
+  (let ((parsed (run-parser (parse-elves) input)))
+    
+    (iter
+      (repeat 3)
+      (for elf in (sort parsed #'>))
+      (summing elf))
+))
