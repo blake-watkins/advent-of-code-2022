f26923223a57c0165d1e79c83cf08d885a1330d4
Blake Watkins <blakewatkins@gmail.com>
2022-12-01 18:14:10 +1300

    Clean up day 1.

--- day1.lisp
+++ day1.lisp
@@ -2,17 +2,17 @@
 
 (defun parse-elf ()
   (with-monad
-    (assign cals (parse-list (parse-number) (parse-newline)))
-    (unit (reduce #'+ cals))))
+    (assign calories (parse-list (parse-number) (parse-newline)))
+    (unit (reduce #'+ calories))))
 
 (defun parse-elves ()
-  (parse-list (parse-elf) (with-monad (parse-newline) (parse-newline))))
+  (parse-list (parse-elf) (n-of 2 (parse-newline))))
 
-(defun day1 (input)
-  (let ((parsed (run-parser (parse-elves) input)))
-    
-    (iter
-      (repeat 3)
-      (for elf in (sort parsed #'>))
-      (summing elf))
-))
+(defun day1 (input &key (part 1))
+  (let ((parsed (sort (run-parser (parse-elves) input) #'>)))
+    (if (= part 1)
+	(apply #'max parsed)
+	(iter
+	  (repeat 3)
+	  (for elf in parsed)
+	  (summing elf)))))
