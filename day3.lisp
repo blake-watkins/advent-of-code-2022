3d8211ef40c7907e4bf4e2f23418da9465a7a035
Blake Watkins <blakewatkins@gmail.com>
2022-12-04 09:32:28 +1300

    Tidy up day 3.

--- day3.lisp
+++ day3.lisp
@@ -1,39 +1,38 @@
 (in-package :aoc-2022)
 
-(defun parse-file ()
-  (parse-lines (one-or-more (parse-alphanumeric))))
+(defun parse-rucksack ()
+  (parse-line (one-or-more (parse-alphanumeric))))
 
-(defun common (rucksack)
+(defun parse-rucksacks ()
+  (one-or-more (parse-rucksack)))
+
+(defun parse-groups ()
+  (one-or-more (n-of 3 (parse-rucksack))))
+
+(defun split-rucksack (rucksack)
   (let* ((half (floor (length rucksack) 2))
          (front (subseq rucksack 0 half))
          (back (subseq rucksack half (length rucksack))))
-    (iter
-      (for c in front)
-      (finding c such-that (member c back)))))
+    (list front back)))
+
+(defun common (rucksacks)
+  (iter
+    (for c in (first rucksacks))
+    (finding c such-that
+             (every (lambda (rucksack) (member c rucksack)) rucksacks))))
 
 (defun priority (c)
   (if (lower-case-p c)
       (+ (- (char-code c) (char-code #\a)) 1)
       (+ (- (char-code c) (char-code #\A)) 27)))
 
-(defun day3 (input)
+(defun day3 (input &key (part 1))
   (reduce #'+
-          (mapcar (lambda (rucksack) (priority (common rucksack)))
-                  (run-parser (parse-file) input))))
+          (if (= part 1)
+              (mapcar (lambda (rucksack)
+                        (priority (common (split-rucksack rucksack))))
+                      (run-parser (parse-rucksacks) input))
+              (mapcar (lambda (rucksacks) (priority (common rucksacks)))
+                      (run-parser (parse-groups) input)))))
 
-(defun parse-file-2 ()
-  (one-or-more (n-of 3 (with-monad
-                         (assign ret (one-or-more (parse-alphanumeric)))
-                         (parse-newline)
-                         (unit ret)))))
 
-(defun common-2 (rucksacks)
-  (iter
-    (for c in (first rucksacks))
-    (finding c such-that (every (lambda (rucksack) (member c rucksack))
-                                rucksacks))))
-
-(defun day3-2 (input)
-  (reduce #'+
-          (mapcar (lambda (rucksacks) (priority (common-2 rucksacks)))
-                  (run-parser (parse-file-2) input))))
