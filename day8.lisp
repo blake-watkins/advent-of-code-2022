bad53724ccb0fd1e0493aa52808e6a93f796483d
Blake Watkins <blakewatkins@gmail.com>
2022-12-12 08:51:47 +1300

    Finish day 8 part 2

--- day8.lisp
+++ day8.lisp
@@ -3,23 +3,59 @@
 (defun parse-file ()
   (parse-lines (one-or-more (parse-digit))))
 
-(defun get-tree (r c trees)
-  (elt (elt trees r) c))
+(defparameter *directions* '((1 0) (-1 0) (0 1) (0 -1)))
 
-(defun visible? (r c max-r max-c trees)
-  (let ((tree (get-tree r c trees)))
-    (or (iter
-	  (for i below r)
-	  (never (>= (get-tree i c trees) tree)))
-	(iter
-	  (for i from (1+ r) below max-r)
-	  (never (>= (get-tree i c trees) tree)))
-	(iter
-	  (for i below c)
-	  (never (>= (get-tree r i trees) tree)))
-	(iter
-	  (for i from (1+ c) below max-c)
-	  (never (>= (get-tree r i trees) tree))))))
+(defun get-tree (pos trees)
+  (destructuring-bind (r c) pos
+    (if (<= 0 r (1- (length trees)))
+	(let ((row (elt trees r)))
+	  (if (<= 0 c (1- (length row)))
+	      (elt row c)
+	      nil))
+	nil)))
+
+(defun visible-trees-in-direction (pos dir trees &key (prev-height -1))
+  (let* ((next-pos (map 'list #'+ pos dir))
+	 (next (get-tree next-pos trees)))
+    (if next
+	(if (>= next prev-height)
+	    (1+ (visible-trees-in-direction next-pos
+					    dir
+					    trees
+					    :prev-height next))
+	    (visible-trees-in-direction next-pos
+					dir
+					trees
+					:prev-height prev-height))
+	0)))
+
+(defun visible? (pos trees)
+  (some (lambda (dir)
+	  (= 0 (visible-trees-in-direction pos
+					   dir
+					   trees
+					   :prev-height (get-tree pos trees))))
+	*directions*))
+
+(defun view-distance-in-direction (height pos dir trees)
+  (let* ((next-pos (map 'list #'+ pos dir))
+	 (next (get-tree next-pos trees)))
+    (if next
+	(if (>= next height)
+	    1
+	    (1+ (view-distance-in-direction height
+					    next-pos
+					    dir
+					    trees)))
+	0)))
+
+(defun scenic-score (pos trees)
+  (apply #'* (mapcar (lambda (dir)
+		       (view-distance-in-direction (get-tree pos trees)
+						   pos
+						   dir
+						   trees))
+		     *directions*)))
 
 (defun day8 (input)
   (let* ((parsed (run-parser (parse-file) input))
@@ -29,5 +65,16 @@
       (for r below max-r)
       (iter
 	(for c below max-c)
-	(when (visible? r c max-r max-c parsed)
+	(when (visible? (list r c) parsed)
 	  (in outer (summing 1)))))))
+
+(defun day8-2 (input)
+  (let* ((parsed (run-parser (parse-file) input))
+	 (max-r (length parsed))
+	 (max-c (length (first parsed))))
+    (iter outer
+      (for r below max-r)
+      (iter
+	(for c below max-c)
+	(in outer (maximizing (scenic-score (list r c) parsed)))))))
+
