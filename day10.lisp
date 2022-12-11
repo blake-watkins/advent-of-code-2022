fb67edccde63d8c405c47158ded5cd2a1fe54967
Blake Watkins <blakewatkins@gmail.com>
2022-12-12 10:18:57 +1300

    Day 10 part 2.

--- day10.lisp
+++ day10.lisp
@@ -16,30 +16,34 @@
 
 (defun process (instructions)
   (iter
-      (with instruction = (pop instructions))
-      (with duration = (second instruction))
-      (with x = 1)
-      (for cycle from 1)
-      (until (and (null instructions) (= 0 duration)))
-      (format t
-              "cycle: ~a instruction: ~a duration: ~a x: ~a~%"
-              cycle
-              (first instruction)
-              duration
-              x)
-      (when (= 20 (mod cycle 40))
-        (collect (list cycle x)))
-      (when (> duration 0)
-        (decf duration))
-      (when (= 0 duration)
-        (ecase (first instruction)
-          (:noop nil)
-          (:addx (incf x (third instruction))))
-        (when instructions
-          (setf instruction (pop instructions))
-          (setf duration (second instruction))))))
+    (with instruction = (pop instructions))
+    (with duration = (second instruction))
+    (with x = 1)
+    (for cycle from 1)
+    (for crt from 0)
+    (until (and (null instructions) (= 0 duration)))
+    (format t
+            "cycle: ~a instruction: ~a duration: ~a x: ~a~%"
+            cycle
+            (first instruction)
+            duration
+            x)
+    (collect (if (<= (abs (- (mod crt 40) x)) 1) #\# #\.))
+    (when (> duration 0)
+      (decf duration))
+    (when (= 0 duration)
+      (ecase (first instruction)
+        (:noop nil)
+        (:addx (incf x (third instruction))))
+      (when instructions
+        (setf instruction (pop instructions))
+        (setf duration (second instruction))))))
 
 (defun day10 (input)
-  (let ((instructions (run-parser (parse-file) input)))
-    (reduce #'+ (mapcar (lambda (s) (reduce #'* s)) (process instructions)))
-    ))
+  (let* ((instructions (run-parser (parse-file) input))
+         (chars (process instructions)))
+    (iter
+      (for r from 0 below 240 by 40)
+      (format t "~{~a~}~%" (subseq chars r (+ r 40))))
+    (length chars)
+    chars))
