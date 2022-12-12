d93103e3e7461def0aa821fe5ca63d4256a39b8d
Blake Watkins <blakewatkins@gmail.com>
2022-12-12 14:19:23 +1300

    Clean up day 9.

--- day9.lisp
+++ day9.lisp
@@ -1,11 +1,7 @@
 (in-package :aoc-2022)
 
 (defun parse-file ()
-  (parse-lines (with-monad
-                 (assign dir (parse-keyword))
-                 (parse-space)
-                 (assign amount (parse-number))
-                 (unit (list dir amount)))))
+  (parse-lines (parse-list (either (parse-number) (parse-keyword)) " ")))
 
 (defun move (pos dir)
   (destructuring-bind (r c) pos
@@ -13,30 +9,14 @@
       (:U (list (1- r) c))
       (:D (list (1+ r) c))
       (:L (list r (1- c)))
-      (:r (list r (1+ c))))))
+      (:R (list r (1+ c))))))
 
 (defun update-tail (head tail)
   (let ((diff (map 'list #'- head tail)))
     (cond
       ((every (lambda (d) (<= (abs d) 1)) diff) tail)
-      (t (map 'list #'+ tail
-              (mapcar (lambda (d)
-                        (if (= 0 d) 0 (/ d (abs d))))
-                      diff))))))
+      (t (map 'list #'+ tail (mapcar #'signum diff))))))
 
-(defun print-grid (head tail)
-  (format nil "~{~a~}~%"
-          (iter
-            (for r from -4 to 0)
-            (collect
-                (format nil "~{~a~}~%"
-                        (iter
-                          (for c from 0 to 5)
-                          (collect (cond
-                                     ((equal (list r c) head) #\H)
-                                     ((equal (list r c) tail) #\T)
-                                     ((equal (list r c) '(0 0)) #\s)
-                                     (t #\.)))))))))
 (defun day9 (input &key (part 1))
   (let ((num-knots (if (= part 1) 2 10))
         (moves (run-parser (parse-file) input)))
@@ -49,7 +29,6 @@
         (setf (elt knots 0) (move (elt knots 0) direction))
         (iter
           (for i from 1 below num-knots)
-          (setf (elt knots i) (update-tail (elt knots (1- i))
-                                           (elt knots i))))
+          (setf (elt knots i) (update-tail (elt knots (1- i)) (elt knots i))))
         (setf (gethash (elt knots (1- num-knots)) tail-positions) t))
       (finally (return (hash-table-count tail-positions))))))
