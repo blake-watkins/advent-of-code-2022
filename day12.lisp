7f18e2650f5d1651f782edad4091eb4080a01fa9
Blake Watkins <blakewatkins@gmail.com>
2022-12-13 09:05:47 +1300

    Use util functions.

--- day12.lisp
+++ day12.lisp
@@ -3,43 +3,43 @@
 (defun parse-file ()
   (parse-lines (parse-list (parse-alphanumeric) "")))
 
-(defun neighbours (cur squares)
-  (remove-if-not (lambda (s)
-                   (let ((from-char (gethash s squares)))
-                     (and from-char
-                          (<= (char-code (gethash cur squares))
-                              (1+ (char-code from-char))))))
-                 (mapcar (lambda (dir) (map 'list #'+ cur dir))
+(defun neighbours (square squares)
+  (remove-if-not (lambda (s) (gethash s squares))
+                 (mapcar (lambda (dir)
+                           (map 'list #'+ square dir))
                          '((0 1) (0 -1) (1 0) (-1 0)))))
 
 (defun get-map (parsed)
-  (iter
-    (with start = nil)
-    (with end = nil)
-    (with squares = (make-hash-table :test 'equal))
-    (for r from 0)
-    (for row in parsed)
+  (let ((map (hash-table-from-list-list parsed))
+        (start nil)
+        (end nil))
     (iter
-      (for c from 0)
-      (for square in row)
-      (setf (gethash (list r c) squares) square)
-      (when (char= square #\S)
-        (setf start (list r c))
-        (setf (gethash (list r c) squares) #\a))
-      (when (char= square #\E)
-        (setf end (list r c))
-        (setf (gethash (list r c) squares) #\z)))
-    (finally (return (list start end squares)))))
+      (for (vertex val) in-hashtable map)
+      (when (char= val #\S) (setf start vertex))
+      (when (char= val #\E) (setf end vertex)))
+    (setf (gethash start map) #\a)
+    (setf (gethash end map) #\z)    
+    (list start end map)))
 
-(defun day12 (input)
-  (let* ((parsed (run-parser (parse-file) input)))
-    (destructuring-bind (start end squares) (get-map parsed)
-      (labels ((neighbour-fn (cur) (neighbours cur squares)))
-        (iter
-          (for (vertex parent distance ign)
-               in-bfs-from end
-               neighbours #'neighbour-fn
-               test 'equal)
-          (format t "~a~%" vertex)
-          (until (char= (gethash vertex squares) #\a))
-          (finally (return distance)))))))
+(defun start-squares (map)
+  (iter
+    (for (vertex val) in-hashtable map)
+    (when (char= val #\a) (collect vertex))))
+
+(defun day12 (input &key (part 1))
+  (destructuring-bind (start end squares)
+      (get-map (run-parser (parse-file) input))
+    (labels ((neighbour-fn (cur)
+               (let ((cur-elevation (char-code (gethash cur squares))))
+                 (remove-if-not
+                  (lambda (n)
+                    (let ((n-elevation (char-code (gethash n squares))))
+                      (<= n-elevation (1+ cur-elevation))))
+                  (neighbours cur squares)))))
+      (iter
+        (for (vertex parent distance)
+             in-bfs-from (if (= part 1) start (start-squares squares))
+             neighbours #'neighbour-fn
+             test 'equal)
+        (until (equal vertex end))
+        (finally (return distance))))))
