471f8944083836f484d979a77e5d62aa68435811
Blake Watkins <blakewatkins@gmail.com>
2022-12-18 08:10:36 +1300

    Speed up part 1 - takes ~2 secs for real input

--- day16.lisp
+++ day16.lisp
@@ -23,17 +23,17 @@
                       :next-valves tunnels
                       :neighbours nil))))
 
-(defun shortest-from (pos valves)
-  (let ((distances (make-hash-table :test 'eq)))
-    (iter
-      (for (cur-vertex parent distance)
-           in-bfs-from pos
-           neighbours (lambda (v)
-                        (valve-next-valves (gethash v valves)))
-           test 'eq
-           single t)
-      (setf (gethash cur-vertex distances) distance))
-    distances))
+(defun parse-file ()
+  (with-monad
+    (assign valves (parse-lines (parse-valve)))
+    (unit (iter
+            (with ret = (make-hash-table))
+            (with to-open = (fset:empty-set))
+            (for valve in valves)
+            (setf (gethash (valve-name valve) ret) valve)
+            (when (> (valve-flow valve) 0)
+              (fset:includef to-open (valve-name valve)))
+            (finally (return ret))))))
 
 (defun set-neighbours (valves to-open)
   (iter
@@ -53,63 +53,24 @@
           (sort (valve-neighbours valve) #'>
                 :key (lambda (neighbour) (valve-flow (gethash (first neighbour) valves)))))))
 
-(defun upper-bound-pressure (time-remaining valve to-open valves)
-  (fset:reduce #'+
-               (fset:image
-                (lambda (v)
-                  (* (valve-flow (gethash v valves))
-                     (max 0 (- time-remaining
-                               (let ((n-dist
-                                       (find-if (lambda (i) (eq (first i) v))
-                                                (valve-neighbours
-                                                 (gethash valve valves)))))
-                                 (if n-dist
-                                     (second n-dist)
-                                     0))))))
-                to-open)))
-
-(defun open-valves (time-remaining pressure valve to-open valves visited)
-;  (break)
+(defun open-valves (time-remaining pressure valve to-open valves)
   (if (or (<= time-remaining 0)
           (fset:empty? to-open))
       pressure
-      (let ((best-pressure pressure))
-        (when (and (fset:contains? to-open valve)
-                   (> (valve-flow (gethash valve valves)) 0))
-          (let ((open-pressure (open-valves (1- time-remaining)
-                                            (+ pressure
-                                               (* (1- time-remaining)
-                                                  (valve-flow (gethash valve valves))))
-                                            valve
-                                            (fset:less to-open valve)
-                                            valves
-                                            (fset:with visited
-                                                       (list valve to-open)))))
-            (setf best-pressure (max best-pressure open-pressure))))
-        (when (> (+ pressure (upper-bound-pressure time-remaining valve to-open valves))
-                 best-pressure)
-          (iter
-            (for (neighbour distance) in (valve-neighbours (gethash valve valves)))
-            (when (and (fset:contains? to-open neighbour)
-                       (not (fset:contains? visited (list neighbour to-open))))
-              (let ((goto-neighbour-pressure
-                      (open-valves (- time-remaining distance)
-                                   pressure
-                                   neighbour
-                                   to-open
-                                   valves
-                                   (fset:with visited
-                                              (list valve to-open)))))
-                (setf best-pressure (max best-pressure goto-neighbour-pressure))))))
-        best-pressure)))
+      (iter
+        (for (neighbour distance) in (valve-neighbours (gethash valve valves)))
+        (for neighbour-open-time = (- time-remaining (+ distance 1)))
+        (when (and (fset:contains? to-open neighbour))
+          (maximizing
+           (open-valves-2 neighbour-open-time
+                          (+ pressure (* neighbour-open-time
+                                         (valve-flow (gethash neighbour valves))))
+                          neighbour
+                          (fset:less to-open neighbour)
+                          valves))))))
 
 (defun day16 (input)
-  (let* ((parsed (run-parser (parse-lines (parse-valve)) input))
-         (valves (iter
-                   (with ret = (make-hash-table))
-                   (for valve in parsed)
-                   (setf (gethash (valve-name valve) ret) valve)
-                   (finally (return ret))))
+  (let* ((valves (run-parser (parse-file) input))
          (to-open (iter
                     (with ret = (fset:empty-set))
                     (for (name valve) in-hashtable valves)
@@ -117,19 +78,4 @@
                       (fset:includef ret name))
                     (finally (return ret)))))
     (set-neighbours valves to-open)
-    (open-valves 30 0 :aa to-open valves (fset:empty-set))))
-
-
-
-(defun memoize (fn)
-  (let ((memo (make-hash-table :test 'equal)))
-    (lambda (&rest args)
-      (multiple-value-bind (ret present) (gethash args memo)
-        (if present
-            ret
-            (progn
-              (let ((store (apply fn args)))
-                (setf (gethash args memo) store)
-                store)))))))
-(defparameter open-valves-memo (memoize #'open-valves))
-
+    (open-valves-2 30 0 :aa to-open valves)))
