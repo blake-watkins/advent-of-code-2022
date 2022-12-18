380cda80f29617405551c0c0f2add8c16b061024
Blake Watkins <blakewatkins@gmail.com>
2022-12-19 08:44:38 +1300

    Day 18 part 2 & cleanup

--- day18.lisp
+++ day18.lisp
@@ -7,16 +7,50 @@
   (mapcar (lambda (d) (point+ point d)) '((1 0 0) (-1 0 0)
                                           (0 1 0) (0 -1 0)
                                           (0 0 1) (0 0 -1))))
-(defun day18 (input)
-  (let ((parsed (run-parser (parse-file) input))
-        (map (make-hash-table :test 'equal)))
-    (iter
-      (for point in parsed)
-      (setf (gethash point map) t))
+
+(defun map-dimensions (map)
+  (iter
+    (with max = nil)
+    (with min = nil)
+    (for (point nil) in-hashtable map)
+    (setf max (point-max max point))
+    (setf min (point-min min point))
+    (finally (return (list min max)))))
+
+(defun get-exterior (map)
+  (destructuring-bind (min max) (map-dimensions map)
+    (setf min (point- min '(1 1 1)))
+    (setf max (point+ max '(1 1 1)))
+    (let ((exterior (make-hash-table :test 'equal)))
+      (labels ((valid-exterior-point (point)
+                 (and (every (lambda (min val max) (<= min val max))
+                             min point max)
+                      (not (gethash point map)))))
+        (iter
+          (for (point) in-bfs-from min             
+               neighbours (lambda (point)
+                            (remove-if-not #'valid-exterior-point
+                                           (neighbours point)))
+               test 'equal
+               single t)
+          (setf (gethash point exterior) t)
+          (finally (return exterior)))))))
+
+(defun day18 (input &key (part 1))
+  (let* ((parsed (run-parser (parse-file) input))
+         (map (iter
+                (with map = (make-hash-table :test 'equal))
+                (for point in parsed)
+                (setf (gethash point map) t)
+                (finally (return map))))
+         (exterior (get-exterior map)))
     (iter
       (for point in parsed)
       (for neighbours = (neighbours point))
       (summing
        (iter
          (for n in neighbours)
-         (counting (not (gethash n map))))))))
+         (counting (and (not (gethash n map))
+                        (or (= part 1) (gethash n exterior)))))))))
+
+
