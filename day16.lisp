9e947f69c8ac261b8960bc11e0eb321cd0e264d7
Blake Watkins <blakewatkins@gmail.com>
2022-12-19 10:52:50 +1300

    Speed improvements
    * Don't use fset
    * Use bit array for TO-OPEN set rather than fset:set or list
    * Use hashtables instead of fset:map
    
    Part 1 down to ~.8s, part 2 down to ~50s

--- day16.lisp
+++ day16.lisp
@@ -28,14 +28,18 @@
     (assign valves (parse-lines (parse-valve)))
     (unit (iter
             (with ret = (make-hash-table))
-            (with to-open = (fset:empty-set))
+            (with to-open = '())
             (for valve in valves)
             (setf (gethash (valve-name valve) ret) valve)
             (when (> (valve-flow valve) 0)
-              (fset:includef to-open (valve-name valve)))
+              (push (valve-name valve) to-open))
             (finally
              (set-neighbours ret to-open)
-             (return (list ret to-open)))))))
+             (return (list ret
+                           (make-array (length to-open)
+                                       :element-type 'bit
+                                       :initial-element 1)
+                           to-open)))))))
 
 (defun set-neighbours (valves to-open)
   (iter
@@ -49,51 +53,72 @@
            test 'eq
            single t)
       (when (and (not (eq name neighbour))
-                 (fset:contains? to-open neighbour))
+                 (member neighbour to-open))
         (push (list neighbour distance) (valve-neighbours valve))))
     (setf (valve-neighbours valve)
           (sort (valve-neighbours valve) #'>
                 :key (lambda (neighbour) (valve-flow (gethash (first neighbour) valves)))))))
 
-(defparameter *cache* (fset:empty-map 0))
+(defparameter *cache* nil)
 
-(defun open-valves (time-remaining valve to-open valves)
-  (let ((ret 0))
-    (unless (or (<= time-remaining 0)
-                (fset:empty? to-open))
-      (if (fset:domain-contains? *cache* (list time-remaining valve to-open))
-          (setf ret
-                (fset:lookup *cache* (list time-remaining valve to-open)))          
-          (iter
-            (for (neighbour distance) in (valve-neighbours (gethash valve valves)))
-            (for neighbour-open-time = (- time-remaining (+ distance 1)))
-            (when (and (fset:contains? to-open neighbour))
-              (maximizing (+ (* neighbour-open-time
-                                (valve-flow (gethash neighbour valves)))
-                             (open-valves neighbour-open-time
-                                          neighbour
-                                          (fset:less to-open neighbour)
-                                          valves))
-                          into rec))
-            (finally (setf ret rec)))))
-    (setf *cache*
-          (fset:with *cache*
-                     (list time-remaining valve to-open)
-                     (max (fset:lookup *cache* (list time-remaining valve to-open))
-                          ret)))
+(defun remove-valve (valve-name to-open valve-names)
+  (let ((ret (copy-seq to-open)))
+    (setf (bit ret (position valve-name valve-names)) 0)
     ret))
 
+(defun valves-open (to-open)
+  (every (lambda (x) (= 0 x)) to-open))
+
+(defun valve-closed (valve to-open valve-names)
+  (= 1 (bit to-open (position valve valve-names))))
+
+(defun list-to-to-open (valves valve-names)
+  (let ((ret (make-array (length valve-names)
+                         :element-type 'bit
+                         :initial-element 0)))
+    (iter
+      (for valve in valves)
+      (setf (bit ret (position valve valve-names)) 1)
+      (finally (return ret)))))
+
+(defun open-valves (key valves valve-names)
+  (destructuring-bind (time-remaining valve to-open) key
+    (let ((ret 0))
+      (unless (or (<= time-remaining 0) (valves-open to-open))
+        (if (gethash key *cache*)
+            (setf ret (gethash key *cache*))
+            (iter
+              (for (neighbour distance) in
+                   (valve-neighbours (gethash valve valves)))
+              (for neighbour-open-time = (- time-remaining (+ distance 1)))
+              (when (valve-closed neighbour to-open valve-names)
+                (maximizing (+ (* neighbour-open-time
+                                  (valve-flow (gethash neighbour valves)))
+                               (open-valves (list  neighbour-open-time
+                                                   neighbour
+                                                   (remove-valve neighbour
+                                                                 to-open
+                                                                 valve-names))
+                                            valves
+                                            valve-names))
+                            into rec))
+              (finally (setf ret rec)))))
+      (setf (gethash key *cache*) (max (gethash key *cache* 0) ret))
+      ret)))
+
 (defun day16 (input &key (part 1))
-  (destructuring-bind (valves to-open) (run-parser (parse-file) input)
+;  (setf *cache* (make-hash-table :test 'equal))
+  (destructuring-bind (valves to-open valve-names) (run-parser (parse-file) input)
     (if (= part 1)
-        (open-valves 30 :aa to-open valves)
+        (open-valves (list  30 :aa to-open) valves valve-names)
         (iter
           (for count from 1)
           (when (zerop (mod count 100))
             (format t "~a ~a~%" count max-score))
-          (for my-valves-list in (combinations (fset:convert 'list to-open)))
-          (for my-valves = (fset:convert 'fset:set my-valves-list))
-          (for elephant-valves = (fset:set-difference to-open my-valves))
-          (for my-score = (open-valves 26 :aa my-valves valves))
-          (for elephant-score = (open-valves 26 :aa elephant-valves valves))
-          (maximizing (+ my-score elephant-score) into max-score)))))
+          (for my-valves-list in (combinations valve-names))
+          (for my-valves = (list-to-to-open my-valves-list valve-names))
+          (for elephant-valves = (bit-not my-valves))
+          (for my-score = (open-valves (list 26 :aa my-valves) valves valve-names))
+          (for elephant-score = (open-valves (list 26 :aa elephant-valves) valves valve-names))
+          (maximizing (+ my-score elephant-score) into max-score)
+          (finally (return max-score))))))
