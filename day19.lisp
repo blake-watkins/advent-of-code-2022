88fad37a98cf3001136e3fc27fb3603811244855
Blake Watkins <blakewatkins@gmail.com>
2022-12-20 14:11:49 +1300

    Finish part 2

--- day19.lisp
+++ day19.lisp
@@ -50,6 +50,8 @@
 (setf (symbol-function 'mineral-) (map-mineral #'-))
 (setf (symbol-function 'mineral>=-int) (map-mineral #'>=))
 (setf (symbol-function 'mineral-max) (map-mineral #'max))
+(setf (symbol-function 'mineral-clamp)
+      (map-mineral (lambda (a b) (if (> b 0) (min a (* 2 b)) a))))
 
 (defun mineral>= (a b)
   (let ((ret (mineral>=-int a b)))
@@ -60,18 +62,16 @@
     (for (type resources) in blueprint)
     (finding resources such-that (eq robot-type type))))
 
-;; build a geode robot if possible
 ;; only build a robot if production of that ore type is less than the maximum
 ;;   required production of that ore type
-(defun available-to-build (resources robots blueprint max-production prevent-types)
+(defun available-to-build (resources robots blueprint max-production)
   (iter
     (for type in *mineral-types*)
     (for required-resources = (required-resources type blueprint))
     (when (and (mineral>= resources required-resources)
                (or (eq type 'geode)
-                   (and (not (member type prevent-types))
-                        (< (slot-value robots type)
-                           (slot-value max-production type)))))
+                   (< (slot-value robots type)
+                      (slot-value max-production type))))
       (collect type))))
 
 (defun max-required-production (blueprint)
@@ -82,56 +82,68 @@
     (setf ret (mineral-max ret required-resources))
     (finally (return ret))))
 
-(defun max-geodes (time-remaining resources robots blueprint max-resources prevent-types)
-  (break)
-  (if (= 0 time-remaining)
-      (mineral-geode resources)
-      (let ((available-to-build (available-to-build resources
-                                                    robots
-                                                    blueprint
-                                                    max-resources
-                                                    prevent-types))
-            (new-resources (mineral+ resources robots)))
-        (if (member 'geode available-to-build)
-            (max-geodes (1- time-remaining)
-                        (mineral- new-resources (required-resources 'geode
-                                                                    blueprint))
-                        (mineral+ robots (make-mineral :geode 1))
-                        blueprint
-                        max-resources
-                        '())                       
-            (max
-             (or (iter
-                   (for type in available-to-build)
-                   (for required-resources = (required-resources type
-                                                                 blueprint))
-                   (maximizing
-                    (max-geodes (1- time-remaining)
-                                (mineral- new-resources required-resources)
-                                (mineral+ robots
-                                          (make-mineral
-                                           (intern (symbol-name type)
-                                                   :keyword)
-                                           1))
-                                blueprint
-                                max-resources
-                                '())))
-                 0)
-             (max-geodes (1- time-remaining)
-                         new-resources
-                         robots
-                         blueprint
-                         max-resources
-                         available-to-build))))))
+(defparameter *cache* (make-hash-table :test 'equalp))
+
+(defun upper-bound (time-remaining resources robots)
+  (+ (mineral-geode resources)
+     (* time-remaining (mineral-geode robots))
+     (/ (* time-remaining (1- time-remaining)) 2)))
+
+(defun max-geodes (time-remaining resources robots blueprint max-resources best-so-far)
+  (if (< (upper-bound time-remaining resources robots) best-so-far)
+      0
+      (or (gethash (list time-remaining resources robots) *cache*)
+	  (let ((ret
+		  (if (= 0 time-remaining)
+		      (mineral-geode resources)
+		      (let* ((available-to-build (available-to-build resources
+								     robots
+								     blueprint
+								     max-resources))
+			     (new-resources (mineral+ resources robots)))
+			(max
+			 (or (iter
+			       (for type in available-to-build)
+			       (for required-resources =
+				    (required-resources type blueprint))
+			       (for best = (max-geodes (1- time-remaining)
+						       (mineral-clamp
+							(mineral- new-resources
+								  required-resources)
+							max-resources)
+						       (mineral-clamp
+							(mineral+ robots
+								  (make-mineral
+								   (intern (symbol-name type)
+									   :keyword)
+								   1))
+							max-resources)
+						       blueprint
+						       max-resources
+						       best-so-far))
+			       (setf best-so-far (max best best-so-far))
+			       (maximizing best))
+			     0)
+			 (max-geodes (1- time-remaining)
+				     (mineral-clamp new-resources max-resources)
+				     robots
+				     blueprint
+				     max-resources
+				     best-so-far))))))
+	    (setf (gethash (list time-remaining resources robots) *cache*)
+		  (max (gethash (list time-remaining resources robots) *cache* 0) ret))))))
 
 (defun day19 (input)
   (let ((parsed (run-parser (parse-file) input)))
     (iter
       (for (id blueprint) in parsed)
       (for max-production = (max-required-production blueprint))
-      (for geodes = (max-geodes 24
+      (setf *cache* (make-hash-table :test 'equalp))
+      (for geodes = (max-geodes 32
                                 (make-mineral)
                                 (make-mineral :ore 1)
                                 blueprint
-                                max-production))
-      (collect (list id geodes (* id geodes))))))
+                                max-production
+				0))
+      (format t "~a ~a ~a~%"  id geodes (* id geodes))
+      (summing (* id geodes)))))
