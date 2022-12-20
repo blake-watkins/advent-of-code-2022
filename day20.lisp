ef8828debdc5f03d2c5cd082c03f8ac181202006
Blake Watkins <blakewatkins@gmail.com>
2022-12-20 20:17:11 +1300

    Tidy up

--- day20.lisp
+++ day20.lisp
@@ -1,60 +1,39 @@
 (in-package :aoc-2022)
 
 (defun parse-file ()
-  (with-monad
-    (assign ret (parse-lines (parse-number)))
-    (unit (fset:convert 'fset:seq ret))))
+  (parse-lines (parse-number)))
 
 (defun mix-num (nums num orig-idx)
-  (let* ((idx (fset:position-if (lambda (pair)
-                                  (= orig-idx (second pair)))
+  (let* ((idx (fset:position-if (lambda (pair) (= orig-idx (second pair)))
                                 nums))
-         (new-idx (mod (+ num (if (= idx (1- (fset:size nums)))
-                                  0
-                                  idx))
-                       (1- (fset:size nums))))
+         (new-idx (mod (+ num idx) (1- (fset:size nums))))
          (without (fset:less nums idx)))
     (fset:insert without new-idx (list num orig-idx))))
 
 (defun mix (nums)
   (iter
     (with ret = nums)
-    (for (num idx) in-fset (fset:sort  nums #'< :key #'second))
+    (for (num idx) in-fset (fset:sort nums #'< :key #'second))
     (setf ret (mix-num ret num idx))
-;;    (format t "~a~%" ret)
     (finally (return ret))))
 
-(defun grove (mixed)
+(defun grove (nums)
   (iter
-    (with pos = (fset:position-if (lambda (pair)
-                                    (= 0 (first pair)))
-                                  mixed))
+    (with zero-pos = (fset:position-if (lambda (pair) (= 0 (first pair))) nums))
     (for idx in '(1000 2000 3000))
-    (sum (first (fset:lookup mixed (mod (+ idx pos) (fset:size mixed)))))))
+    (sum (first (fset:lookup nums (mod (+ zero-pos idx) (fset:size nums)))))))
 
-(defun day20 (input)
-  (let* ((parsed (run-parser (parse-file) input))
-         (indexed
-           (iter
-             (with ret = parsed)
-             (for idx from 0)
-             (for num in-fset parsed)
-             (setf ret (fset:with ret idx (list num idx)))
-             (finally (return ret)))))
-    (grove (mix indexed))))
-
-(defun day20-2 (input)
-  (let* ((parsed (run-parser (parse-file) input))
-         (indexed
-           (iter
-             (with ret = parsed)
-             (for idx from 0)
-             (for num in-fset parsed)
-             (setf ret (fset:with ret idx (list (* num 811589153) idx)))
-             (finally (return ret)))))
-    (iter
-      (repeat 10
-              )
-      (setf indexed (mix indexed)))
+(defun day20 (input &key (part 1))
+  (let* ((nums (mapcar (lambda (num) (* num (if (= part 1) 1 811589153)))
+                       (run-parser (parse-file) input)))
+         (pairs (fset:convert 'fset:seq
+                              (iter
+                                (for idx from 0)
+                                (for num in nums)
+                                (collect (list num idx))))))
     
-    (grove indexed)))
+    (iter
+      (repeat (if (= part 1) 1 10))
+      (setf pairs (mix pairs)))
+    
+    (grove pairs)))
