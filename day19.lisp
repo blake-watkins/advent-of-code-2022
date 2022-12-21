2ef2aba396f0a868c8cbe8aed369ef254166ac81
Blake Watkins <blakewatkins@gmail.com>
2022-12-22 12:05:35 +1300

    Change algorithm to speed up.
    Choose robot type to build next, rather than minute by minute simulation.

--- day19.lisp
+++ day19.lisp
@@ -31,11 +31,9 @@
 (defun parse-file ()
   (parse-lines (parse-blueprint)))
 
+(defparameter *mineral-types* '(ore clay obsidian geode))
 (defstruct mineral
   (ore 0) (clay 0) (obsidian 0) (geode 0))
-
-(defparameter *mineral-types* '(ore clay obsidian geode))
-(defparameter *cache* (make-hash-table :test 'equalp))
 (defparameter *robots*
   (iter
     (with ret = (make-hash-table))
@@ -49,47 +47,63 @@
     (iter
       (with ret = (make-mineral))
       (for slot in '(ore clay obsidian geode))
-      (setf (slot-value ret slot) (funcall fn
-                                           (slot-value a slot)
-                                           (slot-value b slot)))
+      (setf (slot-value ret slot)
+	    (funcall fn (slot-value a slot) (slot-value b slot)))
       (finally (return ret)))))
 
 (setf (symbol-function 'mineral+) (map-mineral #'+))
 (setf (symbol-function 'mineral-) (map-mineral #'-))
 (setf (symbol-function 'mineral-max) (map-mineral #'max))
-(setf (symbol-function 'mineral-clamp)
-      (map-mineral (lambda (a b) (if (> b 0) (min a (* 2 b)) a))))
-
-(setf (symbol-function 'mineral>=-int) (map-mineral #'>=))
-(defun mineral>= (a b)
-  (let ((ret (mineral>=-int a b)))
-    (every (lambda (slot) (slot-value ret slot)) *mineral-types*)))
+(defun mineral* (m k)
+  (let ((ret (make-mineral)))
+    (mapcar (lambda (slot) (setf (slot-value ret slot)
+				 (* k (slot-value m slot))))
+	    *mineral-types*)
+    ret))
 
 (defun required-resources (robot-type blueprint)
   (iter
     (for (type resources) in blueprint)
     (finding resources such-that (eq robot-type type))))
 
-;; only build a robot if production of that ore type is less than the maximum
-;;   required production of that ore type
-(defun available-to-build (resources robots blueprint max-production)
+;; Return the number of minutes until required resources can be collected
+;; NIL if we can't.
+(defun time-to-collect (required resources robots)
+  (labels ((have-production (required robots)
+	     (every (lambda (slot)
+		      (or (<= (slot-value required slot) 0)
+			  (> (slot-value robots slot) 0)))
+		    *mineral-types*)))
+    (let ((required (mineral- required resources)))
+      (when (have-production required robots)
+	(iter
+	  (for type in *mineral-types*)
+	  (maximizing (if (<= (slot-value required type) 0)
+			  0
+			  (ceiling (slot-value required type)
+				   (slot-value robots type)))))))))
+
+;; Return list of robots to build. Each item is a list of robot type, when it'll
+;; be ready, and the required resources.
+;; Don't return robots if we're already at max production (apart from geode)
+;; Don't return robots if they won't produce anything (too little time)
+(defun choose-robot-to-build (time-remaining resources robots blueprint max-production)
   (iter
     (for type in *mineral-types*)
-    (for required-resources = (required-resources type blueprint))
-    (when (and (mineral>= resources required-resources)
-               (or (eq type 'geode)
-                   (< (slot-value robots type)
-                      (slot-value max-production type))))
-      (collect type))))
+    (for required-resource = (required-resources type blueprint))
+    (for time-to-collect = (time-to-collect required-resource resources robots))
+    (when (and (or (eq type 'geode)
+		   (< (slot-value robots type)
+                      (slot-value max-production type)))
+	       (and time-to-collect (<= time-to-collect (- time-remaining 2))))
+      (collect (list type (1+ time-to-collect) required-resource)))))
 
 ;; How many resources we need to be able to build any robot we like
 (defun max-required-production (blueprint)
-  (iter
-    (with ret = (make-mineral))
-    (for type in *mineral-types*)
-    (for required-resources = (required-resources type blueprint))
-    (setf ret (mineral-max ret required-resources))
-    (finally (return ret))))
+  (reduce (lambda (ret type)
+	    (mineral-max ret (required-resources type blueprint)))
+	  *mineral-types*
+	  :initial-value (make-mineral)))
 
 ;; Upper bound on the number of geodes we can make (disregarding resources)
 ;; How many we have + (Geode robots * time) + (Make a Geode robot every min)
@@ -98,49 +112,34 @@
      (* time-remaining (mineral-geode robots))
      (/ (* time-remaining (1- time-remaining)) 2)))
 
-;; Find the max-geodes by building any robot available.
-(defun max-geodes-rec (time-remaining resources new-resources robots blueprint max-resources best-so-far)
-  (iter
-    (for type in (available-to-build resources robots blueprint max-resources))
-    (for required-resources = (required-resources type blueprint))
-    (for child-resources = (mineral- new-resources required-resources))
-    (for child-robots = (mineral+ robots (gethash type *robots*)))
-    (maximizing 
-     (max-geodes
-      (1- time-remaining)
-      (mineral-clamp child-resources max-resources)
-      (mineral-clamp child-robots max-resources)
-      blueprint
-      max-resources
-      best-so-far)
-     into best-child)
-    (setf best-so-far (max best-child best-so-far))
-    (finally (return (or best-child 0)))))
-
 (defun max-geodes (time-remaining resources robots blueprint max-resources best-so-far)
   (cond
     ((< (upper-bound time-remaining resources robots) best-so-far) 0)
-    ((gethash (list time-remaining resources robots) *cache*)
-     (gethash (list time-remaining resources robots) *cache*))
     ((= 0 time-remaining) (mineral-geode resources))
-    (t (let* ((new-resources  (mineral+ resources robots))
-	      (child-best (max-geodes-rec time-remaining
-					  resources
-					  new-resources
-					  robots
-					  blueprint
-					  max-resources
-					  best-so-far))
-	      (wait-best (max-geodes (1- time-remaining)
-				     (mineral-clamp new-resources max-resources)
-				     robots
-				     blueprint
-				     max-resources
-				     (max best-so-far child-best))))
-	 (setf (gethash (list time-remaining resources robots) *cache*)
-	       (max (gethash (list time-remaining resources robots) *cache* 0)
-		    child-best
-		    wait-best))))))
+    (t (let ((options (choose-robot-to-build time-remaining
+					     resources
+					     robots
+					     blueprint
+					     max-resources)))
+	 (if (null options)
+	     (+ (mineral-geode resources)
+		(* (mineral-geode robots) time-remaining))
+	     (iter
+	       (for (type build-time required-resource) in options)
+	       (for new-resources =
+		    (mineral- (mineral+ resources (mineral* robots build-time))
+			      required-resource))
+	       (for new-robots = (mineral+ robots (gethash type *robots*)))
+	       (maximizing
+		(max-geodes (- time-remaining build-time)
+			    new-resources
+			    new-robots
+			    blueprint
+			    max-resources
+			    best-so-far)
+		into best-child)
+	       (setf best-so-far (max best-so-far (or best-child 0)))
+	       (finally (return best-child))))))))
 
 (defun day19 (input &key (part 1))
   (let ((parsed (run-parser (parse-file) input)))
@@ -148,7 +147,6 @@
 	(iter
 	  (for (id blueprint) in parsed)
 	  (for max-production = (max-required-production blueprint))
-	  (setf *cache* (make-hash-table :test 'equalp))
 	  (for geodes = (max-geodes 24
                                     (make-mineral)
                                     (make-mineral :ore 1)
@@ -161,7 +159,6 @@
 	  (repeat 3)
 	  (for (id blueprint) in parsed)
 	  (for max-production = (max-required-production blueprint))
-	  (setf *cache* (make-hash-table :test 'equalp))
 	  (for geodes = (max-geodes 32
 				    (make-mineral)
 				    (make-mineral :ore 1)
