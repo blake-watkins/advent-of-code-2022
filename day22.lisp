d84c04cbfaf86ed58c896cfd0814986dc1f3e6c4
Blake Watkins <blakewatkins@gmail.com>
2023-01-12 20:00:21 +1300

    Comment

--- day22.lisp
+++ day22.lisp
@@ -20,6 +20,7 @@
     (unit (list (hash-table-from-list-list net) path))))
 
 (defun valid-face-corners (pos size net)
+  "Are the corners of a square, sized SIZE starting at POS, in NET and non-blank?"
   (and (> size 0)
        (every (lambda (offset)
                 (let* ((coord (point+ pos (point* (1- size) offset)))
@@ -28,6 +29,7 @@
               '((0 0) (1 0) (0 1) (1 1)))))
 
 (defun get-first-face (net)
+  "Get a list containing the face size, and the rc index of the first face in NET."
   (let ((col (iter
                (for c from 0)
                (while (eq (gethash (list 0 c) net) :blank))
@@ -38,9 +40,19 @@
        (list face-size (list 0 (floor col face-size)))
        such-that (not (valid-face-corners (list 0 col) (1+ face-size) net))))))
 
+;; +x is to the right, +y is into the screen, +z is up. The reference frame is
+;; looking down onto the xy plane, with +y in the up direction and +x in the right.
+;; Forward in this frame is toward -z.
 (defparameter *reference-frame* '(1 0 0 0))
 (defparameter *reference-frame-forward* '(0 0 -1))
 
+;; Each direction stores:
+;; * the rc offset required to move in that direction on the net
+;; * the score for the password at the end
+;; * the axis to rotate around in the reference frame to move in that direction
+;;   on the cube. Direction given by right hand rule, so for :right (0 -1 0) the
+;;   right thumb pointing to -y means the fingers curl anticlockwise, so will turn
+;;   the reference orientation from looking down to looking right.
 (defparameter *direction-info*
     '((:right (0  1) 0 (0 -1 0)) (:down ( 1 0) 1 (-1 0 0))
       (:left  (0 -1) 2 (0  1 0)) (:up   (-1 0) 3 ( 1 0 0))
@@ -53,12 +65,20 @@
   (car (rassoc offset *direction-info* :test 'equal :key #'first)))
 
 (defun turn (frame direction)
+  "Return a frame turned 90 degrees in DIRECTION from FRAME."
   (q-compose frame (q-rotor (/ pi 2) (direction-axis direction))))
 
 (defun frame-forward (frame)
+  "Return the forward vector in FRAME."
   (q-round (q-rotate-vector *reference-frame-forward* frame)))
 
+;; In the reference frame, the RC coordinates of the net map to a square in the
+;; xy plane. For a face size of 4, the (0,0) rc point maps to (0,3,0) xyz and the
+;; (3,3) rc point maps to (3,0,0). The face is surrounded by 1 coordinate in each
+;; direction so that points on different faces will not map to the same 3d
+;; position.
 (defun rc-to-cube (rc frame face-size)
+  "Return the 3D cube position corresponding to the RC position on the base of a cube with faces of size FACE-SIZE in frame FRAME."
   (destructuring-bind (r c) rc
     (let* ((half-length (/ (1- face-size) 2))
            (center (list half-length half-length (1+ half-length)))
@@ -66,6 +86,9 @@
       (q-round
        (point+ (q-rotate-vector (point- xyz center) frame) center)))))
 
+;; Recursively search from the first face returning a hash table of all faces
+;; and the frame that points to them. As we move from face to face on the net,
+;; change the frame to simulate the cube rolling in the appropriate direction.
 (defun get-face-frames (face-size current net
                         &key (face-frames (make-hash-table :test 'equal))
                              (prev nil)
@@ -83,6 +106,8 @@
                        :current-frame neighbour-frame))
     (finally (return face-frames))))
 
+;; Return a hash table mapping each occupied 3d position to the rc coordinate of
+;; the net that it maps to.
 (defun get-cube (face-size face-frames net)
   (iter
     (with cube = (make-hash-table :test 'equal))
@@ -95,6 +120,10 @@
       (setf (gethash cube-pos cube) pos))
     (finally (return cube))))
 
+;; Takes a 3d POSITION, FRAME, and CUBE. Tries to move forward but if we
+;; get to the edge of the cube, turns up and moves forward onto the next side.
+;; Movement is as if we were crawling around inside the cube. Directions are
+;; relative to FRAME. Return the new position and frame.
 (defun next-position (position frame cube)
   (let ((next-position (point+ position (frame-forward frame))))
     (if (gethash next-position cube)
@@ -102,6 +131,8 @@
         (let ((next-frame (turn frame :up)))
           (list (point+ next-position (frame-forward next-frame)) next-frame)))))
 
+;; Given the current FRAME, and the frame for the face as it is on the net, return
+;; the direction that we are travelling from the perspective of the original net.
 (defun find-net-direction (frame face-frame)
   (destructuring-bind (x y)
       (subseq (frame-forward (q-compose (q-reciprocal face-frame) frame)) 0 2)
